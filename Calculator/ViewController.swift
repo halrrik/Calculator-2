5c70857f916386e44e4e6911df60967b45475c01
hanjoes <hanzhou87@gmail.com>
2015-12-24 09:58:23 +0800

    Implemented basic operators.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -44,12 +44,26 @@ class ViewController: UIViewController {
     }
     @IBAction func operate(sender: UIButton) {
         let operation = sender.currentTitle!
+        if userIsTyping {
+            enter()
+        }
         switch operation {
-//        case "+":
-//        case "−":
-//        case "×":
-//        case "÷":
+        case "+":
+            performOperation{$0 + $1}
+        case "−":
+            performOperation{$1 - $0}
+        case "×":
+            performOperation{$0 * $1}
+        case "÷":
+            performOperation{$1 / $0}
         default: break
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if (operandStack.count >= 2) {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
         }
     }
 }
