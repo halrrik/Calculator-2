7da078f18a235b5ecbeab4fc17f7ec3081f9562d
hanjoes <hanzhou87@gmail.com>
2015-12-24 09:43:21 +0800

    Added basic functionality and UI.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsTyping = false
+    var operandStack = [Double]()
+
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsTyping {
+            display.text = display.text! + digit
+        }
+        else {
+            display.text = digit
+            userIsTyping = true
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func enter() {
+        userIsTyping = false
+        operandStack.append(displayValue)
+        print("operandStack = \(operandStack)");
     }
-
-
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        
+        set {
+            display.text = "\(newValue)"
+            userIsTyping = false
+        }
+    }
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        switch operation {
+//        case "+":
+//        case "−":
+//        case "×":
+//        case "÷":
+        default: break
+        }
+    }
 }
 
