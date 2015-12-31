6dac4b4bcf500c2c717849424a0666a0e4fba3ee
hanjoes <hanzhou87@gmail.com>
2015-12-31 21:54:14 +0800

    Added SplitViewController for the overall layout.

---
+++ Calculator/DrawingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DrawingViewController.swift
+//  Calculator
+//
+//  Created by Hanzhou Shi on 12/31/15.
+//  Copyright © 2015 USF. All rights reserved.
+//
+
+import UIKit
+
+class DrawingViewController: UIViewController {
+
+    
+
+}
