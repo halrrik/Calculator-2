869c52be4304b91e78795c341b85c9c01d96f3ba
hanjoes <hanzhou87@gmail.com>
2016-01-01 17:05:33 +0800

    Delegate the data source using DrawingViewController.

--- Calculator/DrawingView.swift
+++ Calculator/DrawingView.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
+// gives all points to be drawn from the data source
+// the coordinate system for the points is centered
+// at the conventional origin, which is the center of 
+// the screen. user might need to do some conversions.
+protocol DataSource: class {
+    func allPointsInXRange(sender: DrawingView, from: CGFloat, to: CGFloat) -> [CGPoint]?
+}
+
 @IBDesignable
 class DrawingView: UIView {
     
     @IBInspectable
-    var scale: CGFloat = 10
+    var scale: CGFloat = 25
+    
+    weak var source: DataSource?
     
     var axesCenter: CGPoint {
         return convertPoint(center, fromView: superview)
@@ -21,6 +31,18 @@ class DrawingView: UIView {
     override func drawRect(rect: CGRect) {
         let drawer = AxesDrawer()
         drawer.drawAxesInRect(bounds, origin: axesCenter, pointsPerUnit: scale)
+        
+        // draw all points fed from data source else we leave the view empty.
+        let minX = bounds.minX - axesCenter.x
+        let maxX = bounds.maxX - axesCenter.x
+        if let points = source?.allPointsInXRange(self, from: minX, to: maxX) {
+            connectAllPoints(points)
+        }
     }
-
+    
+    private func connectAllPoints(points: [CGPoint]) {
+        for point in points {
+            print("connecting: \(point)")
+        }
+    }
 }
--- Calculator/DrawingViewController.swift
+++ Calculator/DrawingViewController.swift
@@ -8,6 +8,32 @@
 
 import UIKit
 
-class DrawingViewController: UIViewController {
+class DrawingViewController: UIViewController, DataSource {
     
+    @IBOutlet weak var drawingView: DrawingView! {
+        didSet {
+            drawingView.source = self
+        }
+    }
+    
+    var op: (Double -> Double)?
+    
+    func allPointsInXRange(sender: DrawingView, from: CGFloat, to: CGFloat) -> [CGPoint]? {
+        guard op != nil else { return nil }
+        
+        print("from: \(from), to: \(to)")
+        var result = [CGPoint]()
+        // dealing with left part
+        for var x = min(from, 0); x <= min(to, 0); x += 1.0 {
+            let y = op!(Double(x))
+            result.append(CGPoint(x: CGFloat(x), y: CGFloat(y)))
+        }
+        // dealing with right part
+        for var x = max(from, 0); x <= max(to, 0); x += 1.0 {
+            let y = op!(Double(x))
+            result.append(CGPoint(x: CGFloat(x), y: CGFloat(y)))
+        }
+        
+        return result
+    }
 }
